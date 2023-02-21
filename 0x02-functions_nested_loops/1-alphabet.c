8c0ed6db25eb547d235419de8c7f05c003ca723f
kavloa <montoxmimo@gmail.com>
2023-02-20 20:24:55 -0800

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+    char lowercase ;
+
+        for (lowercase ='a' ; lowercase <= 'z'; lowercase++);
+        putchar(lowercase);
+        putchar('\n');
+
+
+        return(0);
+}
