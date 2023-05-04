5650c018032d1896491c5c0eaaef906c5854fb0f
kavloa <montoxmimo@gmail.com>
2023-05-04 15:48:12 -0700

    S

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int sum = 0;
-	int len , j ;
-        int dec = 1;
+	unsigned int total = 0;
+	int len, i;
+	int decval = 1;
 
-
-	if (b = NULL)
+	if (!b)
 		return (0);
-	for (len = 0 ; b[len] != ; len++);
 
-	i = (len - 1)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	i = (len - 1);
 	for (; i >= 0; i--)
 	{
-		if (b[i] != '0' && b[i] != 'i')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		if (b[i] == '1' )
-			sum += dec ;
-	       de *=2;
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
 	}
 
-	return (sum);
-	
-}	
-			
-
+	return (total);
+}
